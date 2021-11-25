ffa528b8cc3a40a8fae1740593d4e8f0352e8004
Денис Братчиков <ds.bratchikov@tensor.ru>
2021-11-25 10:04:24 +0300

    first commit

---
+++ udemy/models/product.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsPath = path.join(
+   path.dirname(process.mainModule.filename),
+   'data',
+   'products.json'
+);
+
+const getProductsFromFile = callback => {
+   fs.readFile(productsPath, (error, fileContent) => {
+      if (error) {
+         callback([]);
+      } else {
+         callback(JSON.parse(fileContent));
+      }
+   });
+}
+
+module.exports = class Product {
+   constructor(title) {
+      this.title = title;
+   }
+
+   save() {
+      getProductsFromFile(products => {
+         products.push(this);
+         fs.writeFile(productsPath, JSON.stringify(products), (error) => {
+            console.log(error);
+         });
+      });
+   }
+
+   static fetchAll(callback) {
+      getProductsFromFile(callback);
+   }
+};
